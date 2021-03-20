66d948e169b5a78eef39abca55673849be358100
Jesus Monreal <jesus_3k@live.com.mx>
2021-03-20 17:16:11 -0600

    Improving header's menu and deleting unnecesary classes

--- app/src/main/java/com/example/pdfprime/MainActivity.kt
+++ app/src/main/java/com/example/pdfprime/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.example.pdfprime
 
-import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.example.pdfprime.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.app_bar_main.view.*
+import kotlinx.android.synthetic.main.menu_header.view.*
 
 /**
  * This class is used to hold current fragment
@@ -30,9 +28,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         setSupportActionBar(binding.appBarMain.toolbar)
+        setListeners()
+        setImagesVersion()
+    }
+
+    private fun setImagesVersion() {
         Glide.with(this).load(Uri.parse("file:///android_asset/pdfPrime.png"))
             .into(binding.appBarMain.ivLogotipo)
-        setListeners()
+        val header = binding.navView.getHeaderView(0);
+        Glide.with(this).load(Uri.parse("file:///android_asset/pdfPrime.png"))
+            .into(header.ivMenuHeader)
+        Glide.with(this).load(Uri.parse("file:///android_asset/pdfPrime.png"))
+            .into(binding.appBarMain.ivLogotipo)
+
+        binding.navView.tvVersion.text = packageManager.getPackageInfo(packageName,0).versionName
     }
 
     private fun setListeners(){
